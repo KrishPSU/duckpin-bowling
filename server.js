1dc88171e333597ca1ba051d086f383bd5517dd0
KrishPSU <163033777+KrishPSU@users.noreply.github.com>
2026-05-15 18:29:16 -0400

    reservation UI

--- server.js
+++ server.js
@@ -20,6 +20,9 @@ app.use(express.static(path.join(__dirname, 'app')));
 app.get('/', (req, res) => {
   res.sendFile(path.join(__dirname, 'app', 'index.html'));
 });
+app.get('/reserve', (req, res) => {
+  res.sendFile(path.join(__dirname, 'app', 'reserve.html'));
+});
 
 // Socket.IO
 io.on('connection', (socket) => {
